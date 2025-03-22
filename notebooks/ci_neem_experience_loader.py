77082b1a0af5d05af0e29cdb2192f8a61ae6932e
fkenghagho <fkenghag@uni-bremen.de>
2025-03-22 21:07:25 +0100

    adding notebooks for experience loading

---
+++ notebooks/ci_neem_experience_loader.py
@@ -0,0 +1,9 @@
+from IPython.display import HTML
+
+
+def load():
+	return HTML("""
+	    <video alt="test" controls>
+		<source src="https://github.com/fkenghagho/NeemHub/raw/refs/heads/main/TRACEBOT-NEEMS/ci_experience.mp4" type="video/mp4">
+	    </video>
+	""")
