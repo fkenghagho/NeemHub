5b111fd4e631268e41bdefb50c14d6b9230d0cec
fkenghagho <fkenghag@uni-bremen.de>
2025-03-26 08:46:11 +0100

    adding neem loader notebook

--- notebooks/neem.py
+++ notebooks/neem.py
@@ -1,2 +1,13 @@
+from neem import *
+from utils import open_desktop, open_rvizweb, run_command, create_marker, publish_marker_array, TracyDemo
+import subprocess
+
+
+def start():
+    open_desktop()
+    
 def load():
-    pass
+    run_command('roslaunch tracebot_description launch_tracebot_rviz.launch simulation:=true')
+    
+def play():
+    player_proc = subprocess.Popen(['rosbag', 'play', '-r', '5', '/home/jovyan/workspace/ros/src/Action_XNCWVJEL.bag'])
